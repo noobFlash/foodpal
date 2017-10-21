f344b523c69a41fdfabeec3b900803a6eab10acd
asikarwar <asikarwar007@gmail.com>
2017-10-21 22:15:09 +0530

    controller setup

--- views/test/app.js
+++ views/test/app.js
@@ -3,3 +3,28 @@
     each_bar_width = $(this).attr('aria-valuenow');
     $(this).width(each_bar_width + '%');
   });
+
+var UIController = (function () {
+  var DOMstring = {
+
+  };
+
+  return{
+
+
+  };
+
+});
+
+var DataController = (function () {
+
+
+    return {
+
+
+    };
+});
+
+var controller = (function (DataCtrl,UICtrl) {
+  
+})(DataController,UIController);

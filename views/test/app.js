3241b2b93fdf76c6bbe2e48bc78c8d0b35cec812
asikarwar <asikarwar007@gmail.com>
2017-10-22 13:17:44 +0530

    adding angular2

--- views/test/app.js
+++ views/test/app.js
@@ -7,13 +7,17 @@
 var UIController = (function () {
   var DOMstring = {
     searchButton : '.btn',
-
+    searchData : '.form-control'
 
 
   };
 
   return {
-
+    getInput : function () {
+      return {
+         inputData : document.querySelector(DOMstring.searchData).value,
+      };
+    },
     getDOMstrings : function () {
         return DOMstring;
     }
@@ -23,7 +27,13 @@ var UIController = (function () {
 })();
 
 var DataController = (function () {
+  var DataModel = function (id,inputData) {
+    this.id = id;
+    this.inputData = inputData;
+  };
+  var data = {
 
+  }
 
     return {
 
@@ -36,12 +46,30 @@ var controller = (function (DataCtrl,UICtrl) {
   var setupEventListeners =function () {
     var DOM = UICtrl.getDOMstrings();
 
-    document.querySelector(DOM.searchButton).addEventListener('click',function () {
-      console.log("search");
+    document.querySelector(DOM.searchButton).addEventListener('click',ctrlAddItem);
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
     });
+
 
   };
 
+  var ctrlAddItem = function () {
+    //1. get input data
+    var input;
+    input = UICtrl.getInput();
+    
+    if (input.inputData !== "") {
+      console.log(input);
+    }
+    //2. search for the input data
+    //3. add output data in UI
+    //4. clear Fields
+    //5. add best fit product
+    //6. calculate percentage
+  };
 
 
 
